c207e72f826b46b891554cc66e8bb8a7e680277c
Guilherme <101269920+GuilhermeVoyna@users.noreply.github.com>
2022-04-01 19:22:11 -0300

    atividade sala

--- SimuladoAtividade1/SimuladoAtividade1/src/Conta.java
+++ SimuladoAtividade1/SimuladoAtividade1/src/Conta.java
@@ -1,3 +1,49 @@
-public class Contas {
-    
+public class Conta {
+    //Atributos da nossa classe
+
+    private Usuario usuario;
+    private String numero;
+    private double saldo;
+
+    //Construtor
+    public Conta(String numero, Usuario usuario,Double saldo){
+
+        this.usuario=usuario;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+
+    //Métodos da classe
+    public String getNumero() {
+        return numero;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public String visualizarSaldo(){
+        return String.format("R$%.2f", saldo);
+    }
+
+    public boolean depositar(double valor){
+        if(valor < 0) 
+            return false;
+        this.saldo += valor;
+        return true;
+    }
+
+    public boolean sacar(double valor){
+        if(valor > saldo) return false;
+        if(valor < 0) return false;
+        this.saldo -= valor;
+        return true;
+    }
+
+    public boolean transferirDinheiro(double valor, Conta destino){
+        if(!sacar(valor)) return false;
+        if(!destino.depositar(valor)) return false;
+        return true;
+    }
+
 }
