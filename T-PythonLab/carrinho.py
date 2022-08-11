4354fed91d86eafffc6ba87d4d62a7f040ac171b
GUILHERME DE CAMPOS MELLO GUERRA MARTINS <20.00089-8@maua.br>
2022-08-11 11:04:29 -0300

    pitons

---
+++ T-PythonLab/carrinho.py
@@ -0,0 +1,26 @@
+from turtle import position
+
+
+class Carrinho():
+    #Metodo Construtor
+    def __init__(self):
+        self._itens=[]
+    
+    def get_valor_total(self):
+    
+        total =0
+        for item in self._itens:
+            total+=item.get_valor()
+    
+        return total
+
+    def get_quantidadede_itens(self):
+        return len(self._itens)
+    def adicionar(self,item):
+        self._itens.append(item)
+
+    def remover(self,item):
+        if item in self._itens:
+            self._itens.remove(item)
+        pass
+        
