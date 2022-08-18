1cdda78053d9d05bcb70094a63a20c1344d9a81e
Guilherme <101269920+GuilhermeVoyna@users.noreply.github.com>
2022-08-18 10:56:56 -0300

    Criacao da classe Cartao

--- T-PythonLab/PROJETO-LOJA-ONLINE/modelos/pagamento/cartoes.py
+++ T-PythonLab/PROJETO-LOJA-ONLINE/modelos/pagamento/cartoes.py
@@ -5,9 +5,15 @@ class Cartao(Pagamento):
         self._cvv = cvv
         self._validade = validade
     
-def get_titular(self):
-    return self._titular
-def get_validade(self):
-    return self._validade
-def get_final(self):
-    return self._numero[len(self._numero)-4:]
+    def get_titular(self):
+        return self._titular
+    def get_validade(self):
+        return self._validade
+    def get_final(self):
+        return self._numero[len(self._numero)-4:]
+
+class Debito(Cartao):
+    def __init__(self, numero, titular, validade, cvv):
+        super().__init__(numero, titular, validade, cvv)
+    
+    
